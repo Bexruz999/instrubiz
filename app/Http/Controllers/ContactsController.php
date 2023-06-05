2ca03b9aaf1b49a633db58ee20e54bd1a2f6b94f
Bexruz999 <bexruzfatullayev999@gmail.com>
2023-06-05 16:42:29 +0500

    sitemap view

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Country;
+use App\Models\StoreCategory;
+use App\Models\StoreProducer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 
@@ -14,6 +16,7 @@ class ContactsController extends Controller
         if ($subDomain === 'instrubiz') {$subDomain = 'ae';}
         elseif ($subDomain === 'ae' || $subDomain === 'www' || $subDomain === 'om') {
         return redirect('https://instrubiz.ae/contacts', 301);}
+
         $country = Country::whereCode($subDomain)->first();
         return view('contacts.contacts', ['country' => $country]);
     }
@@ -24,7 +27,10 @@ class ContactsController extends Controller
         if ($subDomain === 'instrubiz') {$subDomain = 'ae';}
         elseif ($subDomain === 'ae' || $subDomain === 'www' || $subDomain === 'om') {
         return redirect('https://instrubiz.ae/contacts', 301);}
-        $country = Country::whereCode($subDomain)->first();
-        return view('contacts.sitemap', ['country' => $country]);
+
+        $countries = Country::get();
+        $categories = StoreCategory::whereHas('products')->inRandomOrder()->take(20)->orderBy('name')->get();
+        $brands = StoreProducer::whereHas('products')->inRandomOrder()->take(20)->orderBy('name')->get();
+        return view('contacts.sitemap', ['countries' => $countries, 'categories' => $categories, 'brands' => $brands]);
     }
 }
--- resources/views/contacts/sitemap-service.blade.php
+++ resources/views/contacts/sitemap-service.blade.php
@@ -1,32 +1,67 @@
 <div class="contactpage pagepadd">
     <div class="container">
         <div class="row">
-            <div class="footer-sidebar footer-2 col-xs-12 col-sm-6 col-md-4">
-                <div class="widget widget_mf-custom-menu">
-                    <h4 class="widget-title">Useful Links</h4>
-                    <div class="custom-menu-area">
-                        <div class="custom-menu custom-menu-1">
-                            <ul class="menu" id="yw2">
-                                <li class="listItem nav_item">
-                                    <a class="listItemLink" href="/">Home</a>
-                                </li>
-                                <li class="listItem nav_item">
-                                    <a class="listItemLink" href="/store/brands">Brands</a>
-                                </li>
-                                <li class="listItem nav_item">
-                                    <a class="listItemLink" href="/store/categories">Categories</a>
-                                </li>
-                                <li class="listItem nav_item">
-                                    <a class="listItemLink" href="/contacts">Contacts</a>
-                                </li>
-                                <li class="listItem nav_item">
-                                    <a class="listItemLink" href="/sitemap">Sitemap</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="clear"></div>
-                    </div>
-                </div>
+            <div class="col-sm-6 col-xs-12 col-md-2">
+                <ul>
+                    <h4 style="margin-top: 30px">Navigation</h4>
+                    <li style="margin-bottom: 5px">
+                        <a class="listItemLink" style="color: #393939;font-weight: bolder;text-decoration: #f7c02d underline 2px;" href="/">Home</a>
+                    </li>
+                    <li style="margin-bottom: 5px">
+                        <a class="listItemLink" style="color: #393939;font-weight: bolder;text-decoration: #f7c02d underline 2px;" href="/store/brands">Brands</a>
+                    </li>
+                    <li style="margin-bottom: 5px">
+                        <a class="listItemLink" style="color: #393939;font-weight: bolder;text-decoration: #f7c02d underline 2px;" href="/store/categories">Categories</a>
+                    </li>
+                    <li style="margin-bottom: 5px">
+                        <a class="listItemLink" style="color: #393939;font-weight: bolder;text-decoration: #f7c02d underline 2px;" href="/contacts">Contacts</a>
+                    </li>
+                    <li style="margin-bottom: 5px">
+                        <a class="listItemLink" style="color: #393939;font-weight: bolder;text-decoration: #f7c02d underline 2px;" href="/sitemap">Sitemap</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-sm-6 col-xs-12 col-md-3">
+                <ul>
+                    <h4 style="margin-top: 30px">Other Countries</h4>
+                    @foreach($countries as $country)
+                        <li>
+                            <a class="listItemLink" style="color: #393939;font-weight: bolder;text-decoration: #f7c02d underline 2px;"
+                               href="https://{{$country->code==='ae'?'':$country->code.'.' }}instrubiz.ae" title="{{$country->name}}" target="_blank">
+                                {!! $country->name !!}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+
+            <div class="col-sm-6 col-xs-12 col-md-3">
+                <ul>
+                    <h4 style="margin-top: 30px">Brands</h4>
+                    @foreach($brands as $brand)
+                        <li>
+                            <a class="listItemLink" style="color: #393939;font-weight: bolder;text-decoration: #f7c02d underline 2px;"
+                               href="/store/brand/{{$brand->slug}}" title="{{$brand->name}}" target=" _blank">
+                                {!! $brand->name !!}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+
+            <div class="col-sm-6 col-xs-12 col-md-4">
+                <ul>
+                    <h4 style="margin-top: 30px">Categories</h4>
+                    @foreach($categories as $category)
+                        <li>
+                            <a class="listItemLink" style="color: #393939;font-weight: bolder;text-decoration: #f7c02d underline 2px;"
+                               href="/">
+                                {!! strlen($category->name) < 30 ? $category->name : substr($category->name, 0, 27) . '..' !!}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
             </div>
         </div>
     </div>
